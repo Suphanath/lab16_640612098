33033ab0e5778256b3a3645c12560a3d90120e30
Siiickzer <suphanath_w@cmu.ac.th>
2022-01-14 12:53:24 +0700

    lab16_1 compeleted

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -1,8 +1,25 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
-
+	int a = 5;
+	string b = "A";
+	string c = b;
+	int *x = &a;
+	string *y = &b;
+	int *z = x;
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl;
+	cout << &a << " " << &b << " " << &c << " " << &x << " " << &y << " " << &z << endl;
+	c = "F";
+	b = c;
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl;
+	*y = "W";
+	c = b;
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl;
+	*x = 6;
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl;
+	*z = 7;
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl;
 	
 	
 	return 0;
